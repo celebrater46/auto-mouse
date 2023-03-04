d0a96990145e0eb9955b2b555dea52dcff56859b
celebrater <tkgm45nbchk@gmail.com>
2023-03-04 20:58:31 +0900

    first commit

---
+++ put-note.ps1
@@ -0,0 +1,27 @@
+﻿# mouse cursor's location
+$x = 960
+$y = 540
+
+# declare .NET Framework
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+
+# declare Windows API
+$signature=@'
+[DllImport("user32.dll",CharSet=CharSet.Auto,CallingConvention=CallingConvention.StdCall)]
+public static extern void mouse_event(long dwFlags, long dx, long dy, long cButtons, long dwExtraInfo);
+'@
+$SendMouseClick = Add-Type -memberDefinition $signature -name "Win32MouseEventNew" -namespace Win32Functions -passThru
+
+# back to the previous window
+# ALT + TAB (move to the previous window)
+[System.Windows.Forms.SendKeys]::SendWait("%{TAB}")
+Start-Sleep -m 50
+
+# move the cursor
+[System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point($x, $y)
+Start-Sleep -m 50
+
+# click L
+$SendMouseClick::mouse_event(0x0002, 0, 0, 0, 0);
+$SendMouseClick::mouse_event(0x0004, 0, 0, 0, 0);
