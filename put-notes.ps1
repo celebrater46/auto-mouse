5f2f360c3973cf7adeb08926b6c9f09b6b758836
celebrater <tkgm45nbchk@gmail.com>
2023-03-04 22:28:45 +0900

    input many random notes successfully

--- put-notes.ps1
+++ put-notes.ps1
@@ -1,21 +1,26 @@
 ﻿# default mouse cursor's location
 $x = 360
-$y = 360
-$xGap = 65
-$yGap = 72
-$interval = 50
+$y = 720
+$xGap = 10
+$yGap = 36
+$interval = 10 # msec
 
 # click L
-function click-L {
+function click-L ($ms) {
     $SendMouseClick::mouse_event(0x0002, 0, 0, 0, 0);
     $SendMouseClick::mouse_event(0x0004, 0, 0, 0, 0);
-    Start-Sleep -m $interval
+    Start-Sleep -m $ms
 }
 
 # move the cursor
-function move-mouse ($mx, $my) {
+function move-mouse ($mx, $my, $ms) {
     [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point($mx, $my)
-    Start-Sleep -m $interval
+    Start-Sleep -m $ms
+}
+
+function vardump ($x, $y) {
+    write-host $x X
+    write-host $y Y
 }
 
 # declare .NET Framework
@@ -32,18 +37,24 @@ $SendMouseClick = Add-Type -memberDefinition $signature -name "Win32MouseEventNe
 # back to the previous window
 # ALT + TAB (move to the previous window)
 [System.Windows.Forms.SendKeys]::SendWait("%{TAB}")
-Start-Sleep -m 50
+Start-Sleep -m 3000
 
-for ($i=0; $i -lt 20; $i++){
-    if ($i -ge 15) {
-        $mx = 
-        Write-Host "1です"
-        move-mouse $x+($i * $xGap) $y+()
-    } elseif ($i -eq 2){
-        Write-Host "2です"
-    } elseif ($i -eq 3){
-        Write-Host "3です"
-    } else {
-        Write-Host "その他です"
-    }
+for ($i=0; $i -le 120; $i++){
+    $x2 = $i * $xGap + $x
+    $y2 = Get-Random -Maximum 800 -Minimum 360
+    # if ($i -ge 15) { $y2 = $y + $yi }
+    # if (($i -ge 5) -And ($i -le 10)) { $y2 = $y + $yi }
+    move-mouse $x2 $y2 $interval
+    click-L $interval
+    vardump $x2 $y2 $interval
 }
+
+Start-Sleep -m 300
+[System.Windows.Forms.SendKeys]::SendWait(" ")
+
+# -eq、 -ieq、 -ceq  ==
+# -ne、 -ine、 -cne  !=
+# -gt、 -igt、 -cgt  >
+# -ge、 -ige、 -cge  >=
+# -lt、 -ilt、 -clt  <
+# -le、 -ile、 -cle  <=
