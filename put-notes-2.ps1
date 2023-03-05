33f64656b57f35f8df067d94b4b340108a1c35d4
celebrater <tkgm45nbchk@gmail.com>
2023-03-05 13:26:35 +0900

    succeeded to put notes randomly on 7th chord

--- put-notes-2.ps1
+++ put-notes-2.ps1
@@ -1,13 +1,16 @@
 ﻿# default mouse cursor's location
 [int] $startX = 84
 [int] $startY = 150
-[int] $endX = 1898
+[int] $endX = 1894
 [int] $endY = 670
 # $scrollBarX = 22 # <- adjust endX
 [int] $maxBar = 4
-[int] $lines = 14 # 7 == 1 octave
+[int] $lines = 7 # 7 == 1 octave
 [int] $fullLines = 24 # 12 == 1 octave
 [int[]] $linesArray = @(0, 2, 4, 5, 7, 9, 11, 12, 14, 16, 17, 19, 21, 23)
+[int[]] $chordsArray = @(0, 4, 7, 11)
+# [int] $linesInChord = $chordsArray.Length
+[bool] $chordMode = $TRUE
 [int] $shortestNote = 8 # 16 == 1/16, 8 == 1/8
 [int] $longestNote = 2
 [int] $notes = $maxBar * $shortestNote
@@ -16,10 +19,11 @@
 [int] $xGap = ($endX-$startX)/$notes
 [int] $yGap = ($endY-$startY)/$fullLines
 [int] $interval = 3 # msec
+[int] $times = 4
 
 # click L
 function click-L ($ms) {
-    $blankProbability = Get-Random -Maximum 300 -Minimum 1 # if the num is less than 100, no note
+    $blankProbability = Get-Random -Maximum 200 -Minimum 101 # if the num is less than 100, no note
     if ($blankProbability -gt 100) {
         $SendMouseClick::mouse_event(0x0002, 0, 0, 0, 0);
         $SendMouseClick::mouse_event(0x0004, 0, 0, 0, 0);
@@ -57,21 +61,33 @@ $SendMouseClick = Add-Type -memberDefinition $signature -name "Win32MouseEventNe
 [System.Windows.Forms.SendKeys]::SendWait("%{TAB}")
 Start-Sleep -m 3000
 
-for ($i=0; $i -le $notes; $i++){
-    [int] $num = Get-Random -Maximum $lines -Minimum 1
-    [int] $x2 = $i * $xGap + $startX
-    # $y2 = $endY - ($yGap * $num)
-    [int] $y2 = $endY - ($yGap * $linesArray[$num-1])
-    # if ($i -ge 15) { $y2 = $y + $yi }
-    # if (($i -ge 5) -And ($i -le 10)) { $y2 = $y + $yi }
-    move-mouse $x2 $y2 $interval
-    click-L $interval
-    # vardump $x2 $y2 $interval
-    Write-Host $num NUM
+function createPhrase ($array, $chordMode) {
+    for ($i=0; $i -lt $notes; $i++){
+        [int] $x2 = $i * $xGap + $startX
+        [int] $y2
+        if ($chordMode) {
+            $num = Get-Random -Maximum $chordsArray.Length -Minimum 0
+            $y2 = $endY - ($yGap * $chordsArray[$num])
+        } else {
+            $num = Get-Random -Maximum $lines -Minimum 1
+            $y2 = $endY - ($yGap * $linesArray[$num-1])
+        }
+        
+        # $y2 = $endY - ($yGap * $num)
+        # [int] $y2 = $endY - ($yGap * $linesArray[$num-1])
+        # if ($i -ge 15) { $y2 = $y + $yi }
+        # if (($i -ge 5) -And ($i -le 10)) { $y2 = $y + $yi }
+        move-mouse $x2 $y2 $interval
+        click-L $interval
+        # vardump $x2 $y2 $interval
+        Write-Host $num NUM
+    }
+    
+    Start-Sleep -m 300
+    [System.Windows.Forms.SendKeys]::SendWait(" ")
 }
 
-Start-Sleep -m 300
-[System.Windows.Forms.SendKeys]::SendWait(" ")
+
 
 # -eq、 -ieq、 -ceq  ==
 # -ne、 -ine、 -cne  !=
